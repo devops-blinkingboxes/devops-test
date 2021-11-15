c20ca2ea4ef03d8625ef8cbfdb6d985f5eba386f
Dev Ops <devops@blinkingboxes.com>
2021-11-15 01:52:03 +0000

    added publish and some TODOs to the build script

--- build_publish.sh
+++ build_publish.sh
@@ -6,8 +6,12 @@
 # there if this was a prod thing and more than one person were developing against this
 # because arguments around "formatting should look like <>!" are best left to machines with lint rules.
 
-# give teams the power to name things
-if [ "$1" = "" ] || [ "$1" = "test" ]; then
+OUTPUT_LOG=/home/"${USER}"/container_build_log;
+DOCKER_HUB_ROOT="devops-blinkingboxes"
+
+# give teams the power to name things;
+# likely a horrible idea @scale- TARGETS https://docs.bazel.build/versions/main/guide.html
+if [ "$1" = "" ] || [ $# -ne 2 ]; then
     echo "you must supply the name of the build;"
     echo "usage:"
     echo "  ${0} <build_name> [test]"
@@ -16,23 +20,36 @@ if [ "$1" = "" ] || [ "$1" = "test" ]; then
     echo "    build_name: name of the build"
     echo "  optional:"
     echo "    test: run this container locally"
+    echo "    publish: publish container to docker hub"
     echo ""
     exit 0
 fi
 
+# use hash of git-head for current build
+build_hash=`git rev-parse HEAD`
+
 build_name=$1;
-echo "building ${build_name}"
-docker build -t ${build_name} .;
+docker build -t ${build_name} . >> "${OUTPUT_LOG}";
 
 # we built, now we wanna spin it up locally to poke at it.
 # Good call, and encouraged for this simple setup.
 if [ "$2" = "test" ]; then
-    docker ps | grep ${build_name};
     # check and see if that build name is currently running locally
     # make it die if so
+    docker ps | grep ${build_name};
     if [ $? -eq 0 ]; then
         docker stop ${build_name};
     fi
+    # ...and now run it
     echo "Running ${build_name} locally for testing"
     docker run -it --rm -d -p 8080:80 --name ${build_name} ${build_name};
+elif [ "$2" = "publish" ]; then
+    echo "publishing..."
+    # TODO(drewconner): add the variables from above here...
+    # get moving for now and def come back to this
+    # tag...
+    deployment="devopsblinkingboxes/devops-test:latest"
+    docker tag webserver ${deployment};
+    # and then publish
+    docker push ${deployment};
 fi
